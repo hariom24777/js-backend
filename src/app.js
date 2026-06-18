95ae7ffe255656c74dba80e85cd706873abe0ea5
hariom24777 <hariompatidar751@gmail.com>
2026-06-18 20:54:43 +0530

    feat: implement user registration endpoint with multer middleware for file uploads

--- src/app.js
+++ src/app.js
@@ -22,6 +22,14 @@ app.use(
 
 app.use(express.static("public"));
 
-app.use(cookieParser())
+app.use(cookieParser());
+
+// import routes
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
 
 export { app };
